19b16f77c55831f58bd6e92db088eb786cd6d5e7
NicoV22 <nbaccar@unal.edu.co>
2023-09-15 20:13:56 -0500

    feat(uart):adding startup message and update gitignore

--- STM32L476RG/Core/Src/main.c
+++ STM32L476RG/Core/Src/main.c
@@ -93,6 +93,7 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  HAL_UART_Transmit(&huart2, (uint8_t*) "Hello_World!\r\n", 14,20);
   while (1)
   {
     /* USER CODE END WHILE */
